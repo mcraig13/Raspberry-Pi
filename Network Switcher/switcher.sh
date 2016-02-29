d263bc6166f877731c3e200a73fa13d92f657733
mcraig13 <mrtogana@gmail.com>
2016-02-29 15:01:23 +0000

    Release v1.1

--- Network Switcher/switcher.sh
+++ Network Switcher/switcher.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-wifiFile="/etc/network/interfaces-wifi"
-hotspotFile="/etc/network/interfaces-hotspot"
+wifiFile="/interfaces-wifi"
+hotspotFile="/interfaces-hotspot"
 
-function setToWiFi {
-    sudo rm /etc/network/interfaces
-    sudo cp interfaces-wifi /etc/network/interfaces
-    sudo ifdown wlan0
-    sudo ifup wlan0
-    xmessage "Network has been set to WiFi." -buttons Thanks
+function setToWifi {
+    	sudo ifdown wlan0
+    	sudo rm /etc/network/interfaces
+    	sudo cp interfaces-wifi /etc/network/interfaces
+    	sudo systemctl daemon-reload
+	sudo service networking restart
+	sudo ifup wlan0
+	xmessage "Network has been set to WiFi." -buttons Thanks
 }
 
-function createWiFi {
+function createWifi {
     echo "auto lo
 iface lo inet loopback
 iface eth0 inet manual
 allow-hotplug wlan0
 iface wlan0 inet static
-        address 10.5.5.1
+        address 192.168.0.40
         netmask 255.255.255.0
-    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" >> interfaces-wifi
+    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" >> /interfaces-wifi
 }
 
 function setToHotspot {
-    sudo rm /etc/network/interfaces
-    sudo cp interfaces-hotspot /etc/network/interfaces
+	sudo rm /etc/network/interfaces
+    	sudo cp interfaces-hotspot /etc/network/interfaces
 }
 
 function createHotspot {
@@ -32,7 +34,7 @@ function createHotspot {
 allow-hotplug wlan0
 iface wlan0 inet static
         address 10.5.5.1
-        netmask 255.255.255.0" >> interfaces-hotspot
+        netmask 255.255.255.0" >> /interfaces-hotspot
 }
 
 function reboot {
@@ -49,20 +51,21 @@ xmessage "Choose from the following options:" -buttons WiFi,Hotspot,Cancel
 RESULT=$?
 
 if [ $RESULT -eq 101 ]; then
-    if [ -f "$file" ]; then
-        setToWifi()
-    else
-        createWifi()
-        setToWifi()
-    fi
+	if [ -f "$wifiFile" ]; then
+        	setToWifi
+    	else
+        	createWifi
+        	setToWifi
+    	fi
 elif [ $RESULT -eq 102 ]; then
-    if [ -f "$file" ]; then
-        setToHotspot()
-    else
-        createHotspot()
-        setToHotspot()
-        reboot()
-    fi
+    	if [ -f "$hotspotFile" ]; then
+        	setToHotspot
+		reboot
+    	else
+        	createHotspot
+        	setToHotspot
+        	reboot
+    	fi
 else
     xmessage "Operation cancelled." -buttons Thanks
 fi
